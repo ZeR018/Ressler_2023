c22f2a6e093ebf54d54c1d88e09f4cdc671c35f4
Eugene <varvarin262@gmail.com>
2025-02-27 14:34:54 +0300

    vdp parallel moving works

--- scr/VanDerPol.py
+++ scr/VanDerPol.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import memory_worker as mem
 import colorama
+import settings as s
+import time
 
 colorama.init()
 
@@ -18,9 +20,9 @@ def phase_converter(phase):
             phase_converter(phase)
         return phase
 
-def solver(IC = [1.9, 0.3, 2.1, -0.5], t_max = 5000):
+def solver(IC = [1.9, -1.9, 2.1, 2.1], t_max = 40000):
     class vdp_params():
-        def __init__(self, mu = 0.001, beta = 0, gamma = 0., delta = 0.1, l = 0.):
+        def __init__(self, mu = 0.001, beta = 0.5, gamma = 0., delta = 0.1, l = 0.02):
             self.mu = mu
             self.delta = delta
             self.beta = beta
@@ -29,28 +31,43 @@ def solver(IC = [1.9, 0.3, 2.1, -0.5], t_max = 5000):
 
     def func_vdp_2_maker(p : vdp_params):
 
-        def dissipative_coup(beta, y_this, y_other):
-            return beta * (y_other - y_this)
+        def dissipative_coup(mu, beta, y_this, y_other):
+            return mu * beta * (y_other - y_this)
         
-        def divide_coup(l, y_this, y_other):
-            return l / (y_this - y_other)
+        def divide_coup1(mu, l, y_this, y_other):
+            return mu * l / (y_other - y_this)
+        
+        def divide_coup2(mu, l, y_this, y_other):
+            return mu * l / (y_this - y_other)
+        
+
+        divide_coup = divide_coup2
 
         def func_vdp_2(t, r):
             print(f'\033[F\033[KCurrent integrate time: {round(t, 1)};')
             x1, y1, x2, y2 = r
             
             dx1 = y1
-            dy1 = p.mu * (1. - x1**2) * y1 - x1 + p.mu * divide_coup(p.l, y2, y1)
+            dy1 = p.mu * (1. - x1**2) * y1 - x1 + divide_coup(p.mu, p.l, y1, y2) + dissipative_coup(p.mu, p.beta, y1, y2) + dissipative_coup(p.mu, p.beta, x1, x2)
             dx2 = y2
-            dy2 = p.mu * (1 + p.gamma - x2**2) * y2 - (1 + p.mu * p.delta) * x2 + p.mu * divide_coup(p.l, y1, y2)
-
+            dy2 = p.mu * (1 + p.gamma - x2**2) * y2 - (1 + p.mu * p.delta) * x2 + divide_coup(p.mu, p.l, y2, y1) + dissipative_coup(p.mu, p.beta, y2, y1) + dissipative_coup(p.mu, p.beta, x2, x1)
+            # dy1 = p.mu * (1. - x1**2) * y1 - x1 - dissipative_coup(p.mu, p.beta, y2, y1)
+            # dy2 = p.mu * (1 + p.gamma - x2**2) * y2 - (1 + p.mu * p.delta) * x2 - dissipative_coup(p.mu, p.beta, y1, y2)
             return [dx1, dy1, dx2, dy2]
         return func_vdp_2
     
+    # Integrate
+    start_solve_time = time.time()
+    print('Start solve time:', mem.hms_now())
+
+    print('Start')
     params = vdp_params()
     rhs = func_vdp_2_maker(params)
-    sol = solve_ivp(rhs, [0, t_max], IC, method='RK45', rtol=1e-5, atol=1e-5)
-    print(sol.status)
+    sol = solve_ivp(rhs, [0, t_max], IC, method='RK45', rtol=1e-4, atol=1e-4, max_step=0.05)
+    print('Integrate status:', sol.status)
+
+    time_after_integrate = time.time()
+    print(f'Integrate time: {(time.time() - start_solve_time):0.1f}s', 'time:', mem.hms_now())
 
     xs = [sol.y[0], sol.y[2]]
     ys = [sol.y[1], sol.y[3]]
@@ -58,32 +75,50 @@ def solver(IC = [1.9, 0.3, 2.1, -0.5], t_max = 5000):
     
     size = len(ts)
 
-    plt.plot(ts, xs[0], label='1')
-    plt.plot(ts, xs[1], label='2')
-    plt.grid()
-    plt.legend()
-    plt.xlabel('t')
-    plt.ylabel('x')
-    plt.show()
+    dir_path = s.grid_experiments_path + f'vdp_b_{params.beta}_l_{params.l}_{t_max}'
+    try:
+        mem.make_dir(dir_path)
+    except Exception:
+        print('Dir is already exists') 
 
-    # plt.plot(ts, ys[0], label='1')
-    # plt.plot(ts, ys[1], label='2')
-    # plt.grid()
-    # plt.xlabel('t')
-    # plt.ylabel('y')
-    # plt.legend()
-    # plt.show()
+    def plot_timeline_graph(x : list, t : list, ylabel : str, save_name : str = '', path_save : str = dir_path, 
+                              figsize_ : list=[12, 3], xlabel : list='t', xlims : list = [0, t_max],
+                              subplots_adjust : list=[0.11, 0.97, 0.225, 0.97], 
+                              format : str='.png', x2 : list=[], font_size : int = 18) -> None:
+            plt.figure(figsize=figsize_)
+            plt.subplots_adjust(left=subplots_adjust[0], right=subplots_adjust[1], bottom=subplots_adjust[2], top=subplots_adjust[3])
+            if len(x2) == 0:
+                plt.plot(t, x)
+            else:
+                plt.plot(t, x, label='1')
+                plt.plot(t, x2, label='2')
+                plt.legend()
+            plt.grid()
+            plt.xlabel(xlabel, fontsize=font_size)
+            plt.ylabel(ylabel, fontsize=font_size)
+            plt.xticks(fontsize=font_size)
+            plt.yticks(fontsize=font_size)
+            plt.xlim(xlims[0], xlims[1])
+            if save_name == '':
+                plt.show()
+            else:
+                plt.savefig(path_save + '/' + save_name + '.png')
+            plt.close()
+
+    t_100 = np.searchsorted(ts, 100)
+    plot_timeline_graph(xs[0][:t_100], ts[:t_100], 'x', 'vdp_xt_first_100', x2=xs[1][:t_100], xlims=[0, 100])
+    plot_timeline_graph(ys[0][:t_100], ts[:t_100], 'y', 'vdp_yt_first_100', x2=ys[1][:t_100], xlims=[0, 100])
+
+
+    t_min_100 = np.searchsorted(ts, t_max-100)
+    plot_timeline_graph(xs[0][t_min_100:], ts[t_min_100:], 'x', 'vdp_xt_last_100', x2=xs[1][t_min_100:], xlims=[t_max-100, t_max])
+    plot_timeline_graph(ys[0][t_min_100:], ts[t_min_100:], 'y', 'vdp_yt_last_100', x2=ys[1][t_min_100:], xlims=[t_max-100, t_max])
 
     # Dists
     dists = []
     for i in range(size):
         dists.append(np.sqrt((xs[0][i]-xs[1][i])**2+(ys[0][i]-ys[1][i])**2))
-    plt.figure(figsize=[12, 3])
-    plt.plot(ts, dists)
-    plt.grid()
-    plt.xlabel('t')
-    plt.ylabel('dist')
-    plt.show()
+    plot_timeline_graph(dists, ts, 'd', 'vdp_dist')
 
     # phases diff
     phases_diff = []
@@ -91,12 +126,7 @@ def solver(IC = [1.9, 0.3, 2.1, -0.5], t_max = 5000):
         phase1 = np.arctan2(ys[0][i], xs[0][i])
         phase2 = np.arctan2(ys[1][i], xs[1][i])
         phases_diff.append(phase_converter(phase1 - phase2))
-    plt.figure(figsize=[12, 3])
-    plt.plot(ts, phases_diff)
-    plt.grid()
-    plt.xlabel('t')
-    plt.ylabel(r'$\phi_1 - \phi_2$')
-    plt.show()
+    plot_timeline_graph(phases_diff, ts, r'$\phi_1 - \phi_2$', 'vdp_phases_diff')
 
     # amplitudes diff
     As_diff = []
@@ -107,20 +137,23 @@ def solver(IC = [1.9, 0.3, 2.1, -0.5], t_max = 5000):
         As[0].append(A1)
         As[1].append(A2)
         As_diff.append(A1 - A2)
-    plt.figure(figsize=[12, 3])
-    plt.plot(ts, As_diff)
-    plt.grid()
-    plt.xlabel('t')
-    plt.ylabel(r'A_1 - A_2')
-    plt.show()
+    plot_timeline_graph(As_diff, ts, r'$A_1 - A_2$', 'vdp_As_diff')
+    plot_timeline_graph(As[0], ts, r'$A_1, A_2$', 'vdp_As', x2=As[1])
 
-    plt.figure(figsize=[12, 3])
-    plt.plot(ts, As[0], label=r'A_1')
-    plt.plot(ts, As[1], label=r'A_2')
-    plt.legend()
-    plt.grid()
-    plt.xlabel('t')
-    plt.ylabel(r'A_1, A_2')
-    plt.show()
-    
+    # Считаем метрики
+    # transition_process = int(t_max / 2)
+    transition_process = 10000
+    if t_max >= 25000:
+        transition_process = 15000
+    if t_max > 30000:
+        transition_process = 20000
+    transition_process_time = np.searchsorted(ts, transition_process)
+    diff_phases_average = np.mean(phases_diff[transition_process_time:])
+    diff_As_average = np.mean(As_diff[transition_process_time:])
+    # записываем
+    with open(dir_path + '/metrix.txt', 'w') as f:
+        print('transition process:', transition_process, file=f)
+        print('diff phases avg:', diff_phases_average, file=f)
+        print('diff As avg:', diff_As_average, file=f)
 solver()
+
--- scr/make_some_graphs.py
+++ scr/make_some_graphs.py
@@ -66,19 +66,61 @@ end_t = 3
 # plt.show()
 
 # make omega a graph
-with open('./data/grid_experiments/2024-12-06 13.53.48 s_151_ tau_1/figsomegas.txt', 'r') as f:
-    f_data = f.readlines()
-data = f_data[0][1:-3].split(',')
-data = [float(i) for i in data]
-size = len(data)
-a_arr = np.arange(0.15, 0.3001, 0.001)
+# with open('./data/grid_experiments/2024-12-06 13.53.48 s_151_ tau_1/figsomegas.txt', 'r') as f:
+#     f_data = f.readlines()
+# data = f_data[0][1:-3].split(',')
+# data = [float(i) for i in data]
+# size = len(data)
+# a_arr = np.arange(0.15, 0.3001, 0.001)
 
-plt.figure(figsize=[12, 4])
-plt.plot(a_arr, data)
-plt.xlabel('a', fontsize=28)
-plt.ylabel(r'$\Omega$', fontsize=28)
-plt.xticks([0.15, 0.2, 0.25, 0.3], fontsize=24)
-plt.yticks([2.4, 2.0, 1.6, 1.2], fontsize=24)
-plt.xlim(0.15, 0.30)
+# plt.figure(figsize=[12, 4])
+# plt.plot(a_arr, data)
+# plt.xlabel('a', fontsize=28)
+# plt.ylabel(r'$\Omega$', fontsize=28)
+# plt.xticks([0.15, 0.2, 0.25, 0.3], fontsize=24)
+# plt.yticks([2.4, 2.0, 1.6, 1.2], fontsize=24)
+# plt.xlim(0.15, 0.30)
 
+# plt.show()
+
+
+# последовательное и параллельное движение в зависимости от T
+T_arr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+R1 = 10
+R2 = 4
+phi_xy_arr_1 = [0.6847835068041368, -0.00024049863673546046, 0.016399686986767355, -0.0014154269900184453, -0.010997210275395797, 0.031097546132844593, 0.025533781682397793]
+phi_dxdy_arr_1 = [0.6375898236046866, 0.31648827953012715, -0.25693484402494227, 0.30962105818645375, 0.2743782522851758, -0.25893996038311884, -0.2457769594281952]
+A_arr_1 = [4.455478062375993, 1.5124195579256943, 1.7261598711883346, 2.2042859582012393, 2.453346620407209, 2.5493922605254595, 3.0109133651860933]
+phi_xy_arr_2 = [0.11376065961169111, 0.057988421391588676, 0.03933245767994036, 0.029402790892718744, 0.023948687061699826, 0.020171091165224735, 0.01726948444268236]
+phi_dxdy_arr_2 = [0.11271029305157491, 0.059480732721169244, 0.04093742135830397, 0.02980063878226318, 0.025376289001301476, 0.021079473748354595, 0.018059385011073077]
+A_arr_2 = [0.16318678883448445, 0.06658616698732485, 0.0383475592670401, 0.027268424041928974, 0.02005159101466687, 0.013773590437628437, 0.013545272988249132]
+
+plt.plot(T_arr[1:], phi_xy_arr_1[1:], label='параллельная связь 1')
+plt.plot(T_arr[1:], phi_dxdy_arr_1[1:], label='параллельная связь 2')
+plt.xlabel('t', fontsize=18)
+plt.ylabel(r'$\phi_1 - \phi_2$', fontsize=18)
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(T_arr[1:], phi_xy_arr_2[1:], label='последовательная связь 1')
+plt.plot(T_arr[1:], phi_dxdy_arr_2[1:], label='последовательная связь 2')
+plt.xlabel('t', fontsize=18)
+plt.ylabel(r'$\phi_1 - \phi_2$', fontsize=18)
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(T_arr[1:], A_arr_1[1:], label='параллельная связь')
+plt.xlabel('t', fontsize=18)
+plt.ylabel(r'$A_1 - A_2$', fontsize=18)
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(T_arr[1:], A_arr_2[1:], label='последовательная связь')
+plt.xlabel('t', fontsize=18)
+plt.ylabel(r'$A_1 - A_2$', fontsize=18)
+plt.legend()
+plt.grid()
 plt.show()
